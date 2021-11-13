adc60266fc8f0998170935a61b33d6f35d25cf4e
calebcheng00 <50129617+calebcheng00@users.noreply.github.com>
2021-11-13 10:22:24 +0800

    moved testnft to own dapp

--- src/Testnft.sol
+++ src/Testnft.sol
@@ -1,4 +1,21 @@
-pragma solidity ^0.8.6;
+pragma solidity ^0.7.6;
 
-contract Testnft {
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract Testnft is ERC721 {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    constructor() public ERC721("piccAIso", "CPPN"){
+        _setBaseURI("ipfs://");
+    }
+
+    function mint(address sender, string memory metadataURI) public returns (uint256) {
+        _tokenIds.increment();
+        uint newId = _tokenIds.current();
+        _mint(sender, newId);
+        _setTokenURI(newId, metadataURI);
+        return newId;
+    }
+
 }
