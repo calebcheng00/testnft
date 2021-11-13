a343898fb2020ef2793908ade7e33cd83696a04d
calebcheng00 <50129617+calebcheng00@users.noreply.github.com>
2021-11-13 10:05:55 +0800

    dapp init Testnft

---
+++ src/Testnft.t.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.8.6;
+
+import "ds-test/test.sol";
+
+import "./Testnft.sol";
+
+contract TestnftTest is DSTest {
+    Testnft testnft;
+
+    function setUp() public {
+        testnft = new Testnft();
+    }
+
+    function testFail_basic_sanity() public {
+        assertTrue(false);
+    }
+
+    function test_basic_sanity() public {
+        assertTrue(true);
+    }
+}
